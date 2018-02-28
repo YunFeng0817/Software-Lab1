51328689979c3a2edc67ebae358f907222d3bed4
Fitz <f18846188605@gmail.com>
2018-02-28 20:53:23 +0800

    初步判断了各个矩阵的和是否相等，还未对各种情况处理

--- src/P1/MagicSquare.java
+++ src/P1/MagicSquare.java
@@ -6,28 +6,64 @@ public class MagicSquare {
 
     public static void main(String[] args) {
         // TODO
-        System.out.println("hello world");
-        isLegalMagicSquare("./src/P1/txt/1.txt");
+        System.out.println(isLegalMagicSquare("./src/P1/txt/1.txt"));
+        System.out.println(isLegalMagicSquare("./src/P1/txt/2.txt"));
+        System.out.println(isLegalMagicSquare("./src/P1/txt/3.txt"));
+        System.out.println(isLegalMagicSquare("./src/P1/txt/4.txt"));
+        System.out.println(isLegalMagicSquare("./src/P1/txt/5.txt"));
     }
 
-    private static boolean isLegalMagicSquare(String fileName){
+    private static boolean isLegalMagicSquare(String fileName) {
         // TODO
-        try{
+        final int LENGTH = 1000;
+        int[][] MagicSquare = new int[LENGTH][LENGTH];
+        int[] ColumnSum = new int[LENGTH], RowSum = new int[LENGTH];
+        int DiagonalPositive = 0, DiagonalNegative = 0;
+        int count = 0;
+        try {
             BufferedReader content = new BufferedReader(new FileReader(fileName));
             String line = content.readLine();
-            while(line!=null){
-                String[] parseNum = line.split(" ");
-                System.out.println();
+            while (line != null) {
+                String[] ParseNum = line.split("\t");
+                for (int i = 0; i < ParseNum.length; i++) {
+                    MagicSquare[count][i] = Integer.parseInt(ParseNum[i]);
+                    RowSum[count] += MagicSquare[count][i];
+                    ColumnSum[i] += MagicSquare[count][i];
+                    if (i == count) {
+                        DiagonalNegative += MagicSquare[count][i];
+                    }
+                    if (i + count +1 == ParseNum.length) {
+                        DiagonalPositive += MagicSquare[count][i];
+                    }
+                }
+                count++;
                 line = content.readLine();
             }
-        }
-        catch (Exception e){
+            boolean flag = true;
+            for (int i = 0; i < count - 1; i++) {
+                if (ColumnSum[i] != ColumnSum[i + 1]) {
+                    flag = false;
+                    break;
+                }
+                if (RowSum[i] != RowSum[i + 1]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                if (RowSum[0] == DiagonalPositive) {
+                    if (RowSum[0] == DiagonalNegative)
+                        return true;
+                }
+            }
+            return false;
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return true;
+        return false;
     }
 
-    static void generateMagicSquare(int n){
+    static void generateMagicSquare(int n) {
 
     }
 

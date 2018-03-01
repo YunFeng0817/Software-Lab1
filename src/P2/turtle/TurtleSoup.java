4103473a8fa6460a106f33884ffc27f0676bdd48
Fitz <f18846188605@gmail.com>
2018-03-01 15:08:04 +0800

    finish problem 6 of P2

--- src/P2/turtle/TurtleSoup.java
+++ src/P2/turtle/TurtleSoup.java
@@ -10,12 +10,12 @@ public class TurtleSoup {
 
     /**
      * Draw a square.
-     * 
-     * @param turtle the turtle context
+     *
+     * @param turtle     the turtle context
      * @param sideLength length of each side
      */
     public static void drawSquare(Turtle turtle, int sideLength) {
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             turtle.forward(sideLength);
             turtle.turn(-90);
         }
@@ -23,62 +23,65 @@ public class TurtleSoup {
 
     /**
      * Determine inside angles of a regular polygon.
-     * 
+     * <p>
      * There is a simple formula for calculating the inside angles of a polygon;
      * you should derive it and use it here.
-     * 
+     *
      * @param sides number of sides, where sides must be > 2
      * @return angle in degrees, where 0 <= angle < 360
      */
     public static double calculateRegularPolygonAngle(int sides) {
-        throw new RuntimeException("implement me!");
+        return 180 - (double) 360 / sides;
     }
 
     /**
      * Determine number of sides given the size of interior angles of a regular polygon.
-     * 
+     * <p>
      * There is a simple formula for this; you should derive it and use it here.
      * Make sure you *properly round* the answer before you return it (see java.lang.Math).
      * HINT: it is easier if you think about the exterior angles.
-     * 
+     *
      * @param angle size of interior angles in degrees, where 0 < angle < 180
      * @return the integer number of sides
      */
     public static int calculatePolygonSidesFromAngle(double angle) {
-        throw new RuntimeException("implement me!");
+        return 360 / (int) (180 - angle);
     }
 
     /**
      * Given the number of sides, draw a regular polygon.
-     * 
+     * <p>
      * (0,0) is the lower-left corner of the polygon; use only right-hand turns to draw.
-     * 
-     * @param turtle the turtle context
-     * @param sides number of sides of the polygon to draw
+     *
+     * @param turtle     the turtle context
+     * @param sides      number of sides of the polygon to draw
      * @param sideLength length of each side
      */
     public static void drawRegularPolygon(Turtle turtle, int sides, int sideLength) {
-        throw new RuntimeException("implement me!");
+        for (int i = 0; i < sides; i++) {
+            turtle.forward(sideLength);
+            turtle.turn(180 - calculateRegularPolygonAngle(sides));
+        }
     }
 
     /**
      * Given the current direction, current location, and a target location, calculate the heading
      * towards the target point.
-     * 
+     * <p>
      * The return value is the angle input to turn() that would point the turtle in the direction of
      * the target point (targetX,targetY), given that the turtle is already at the point
      * (currentX,currentY) and is facing at angle currentHeading. The angle must be expressed in
-     * degrees, where 0 <= angle < 360. 
-     *
+     * degrees, where 0 <= angle < 360.
+     * <p>
      * HINT: look at http://en.wikipedia.org/wiki/Atan2 and Java's math libraries
-     * 
+     *
      * @param currentHeading current direction as clockwise from north
-     * @param currentX current location x-coordinate
-     * @param currentY current location y-coordinate
-     * @param targetX target point x-coordinate
-     * @param targetY target point y-coordinate
+     * @param currentX       current location x-coordinate
+     * @param currentY       current location y-coordinate
+     * @param targetX        target point x-coordinate
+     * @param targetY        target point y-coordinate
      * @return adjustment to heading (right turn amount) to get to target point,
-     *         must be 0 <= angle < 360
+     * must be 0 <= angle < 360
      */
     public static double calculateHeadingToPoint(double currentHeading, int currentX, int currentY,
                                                  int targetX, int targetY) {
@@ -88,16 +91,16 @@ public class TurtleSoup {
     /**
      * Given a sequence of points, calculate the heading adjustments needed to get from each point
      * to the next.
-     * 
+     * <p>
      * Assumes that the turtle starts at the first point given, facing up (i.e. 0 degrees).
      * For each subsequent point, assumes that the turtle is still facing in the direction it was
      * facing when it moved to the previous point.
      * You should use calculateHeadingToPoint() to implement this function.
-     * 
+     *
      * @param xCoords list of x-coordinates (must be same length as yCoords)
      * @param yCoords list of y-coordinates (must be same length as xCoords)
      * @return list of heading adjustments between points, of size 0 if (# of points) == 0,
-     *         otherwise of size (# of points) - 1
+     * otherwise of size (# of points) - 1
      */
     public static List<Double> calculateHeadings(List<Integer> xCoords, List<Integer> yCoords) {
         throw new RuntimeException("implement me!");
@@ -105,10 +108,10 @@ public class TurtleSoup {
 
     /**
      * Draw your personal, custom art.
-     * 
+     * <p>
      * Many interesting images can be drawn using the simple implementation of a turtle.  For this
      * function, draw something interesting; the complexity can be as little or as much as you want.
-     * 
+     *
      * @param turtle the turtle context
      */
     public static void drawPersonalArt(Turtle turtle) {
@@ -117,15 +120,15 @@ public class TurtleSoup {
 
     /**
      * Main method.
-     * 
+     * <p>
      * This is the method that runs when you run "java TurtleSoup".
-     * 
+     *
      * @param args unused
      */
     public static void main(String args[]) {
         DrawableTurtle turtle = new DrawableTurtle();
 
-        drawSquare(turtle, 40);
+        drawRegularPolygon(turtle, 500, 1);
 
         // draw the window
         turtle.draw();
